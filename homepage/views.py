8fd26e16d3febcf007595f9d74fc280cee42db5b
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-15 12:40:32 +0200

    Update UI to show database

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
-def index(request):    return render(request, 'homepage/index.html', {})
+def index(request):    
+    products = Product.objects.all()  
+    return render(request, 'homepage/index.html', {'products': products})
