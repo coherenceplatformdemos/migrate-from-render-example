6115aba64dc16fc420c493b77c356e07121a8297
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-15 16:20:31 +0200

    Debug False

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('CNC_ENVIRONMENT_DOMAIN'),
